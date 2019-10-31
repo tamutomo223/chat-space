4603227aa65d872481d5ea91ee4f917920bcdcc5
tamutomo223 <57092560+tamutomo223@users.noreply.github.com>
2019-10-31 19:37:40 +0900

    message_controller_branch

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   resources :users, only: [:edit, :update]
   resources :groups, only: [:new, :create, :edit, :update] do
-    resources :messages, only: [:index]
+    resources :messages, only: [:index, :create]
   end
 
 end
